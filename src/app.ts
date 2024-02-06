5e843f46754aa4d58b42953dd9480899dd4d3871
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-02-06 15:14:43 -0300

    Criação de estrutura de pastas, implementação do Helmet e  organização dos arquivos .env

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
+import "reflect-metadata";
+import "express-async-errors"
+import "dotenv/config"
+import helmet from "helmet";
 import express, { json } from "express";
 
 export const app = express();
 
+app.use(helmet())
 app.use(json());
